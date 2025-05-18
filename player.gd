a4007f4b98f78fbf47c772a4aaae9f6fce1ef0f6
Kekkles1 <fatimamahmood13@outlook.com>
2025-05-18 19:49:26 +0500

    Added animation, tileset

--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@onready var sprite = $AnimatedSprite2D
 
 var can_move = true
 var interactable = null
@@ -9,6 +10,14 @@ var interaction_cooldown = 0.0
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * speed
+	
+	if input_direction != Vector2.ZERO:
+		sprite.play("run")
+		
+		if input_direction.x != 0:
+			sprite.flip_h = input_direction.x < 0  # flip left if moving left
+	else:
+		sprite.stop()
 
 func _physics_process(_delta):
 	if can_move:
@@ -23,6 +32,7 @@ func _process(delta):
 		interaction_cooldown -= delta
 
 	if Input.is_action_just_pressed("interact") and interactable != null and interaction_cooldown <= 0:
+		sprite.play("talk")
 		var dialog_box = get_node("/root/Main/DialogueBox")
 		if not dialog_box.active:
 			interactable.interact()
