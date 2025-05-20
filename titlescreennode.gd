b8e0acf6d00414e44a61b063ecf0b936f8b31ddc
Kekkles1 <fatimamahmood13@outlook.com>
2025-05-20 16:28:30 +0500

    Added a tutorial screen

--- titlescreennode.gd
+++ titlescreennode.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@onready var message_box = $MessageBox
+@onready var play_button = $VBoxContainer/PlayButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,6 +12,16 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
+		
 func _on_play_button_pressed() -> void:
+	message_box.visible = true
+	play_button.disabled = true
+
+
+func _on_message_box_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_level_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
